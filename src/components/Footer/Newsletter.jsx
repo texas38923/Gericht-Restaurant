2b622f7aadc97a0f1380bc36520ad817b5d00e98
Anurag Kamra <anuragkamra7@gmail.com>
2024-01-07 23:38:03 +0530

    final

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
 import './Newsletter.css';
+import SubHeading from './../SubHeading/SubHeading';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title='Newsletter' />
+      <h1 className='headtext__cormorant'>Subscribe to our newsletter</h1>
+      <p className='p__opensans'>And never miss an update from us</p>
+    </div>
+
+    <div className='app__newsletter-input flex__center'>
+      <input
+        type='email'
+        placeholder='Enter your email address...'
+        name='newsletter'
+      />
+      <button type='button' className='custom__button'>
+        Subscribe
+      </button>
+    </div>
   </div>
 );
 
